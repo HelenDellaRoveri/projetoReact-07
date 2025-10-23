c838de7a7c3f54aec2b70a6f86083532857d4377
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2025-10-23 07:26:17 -0300

    Add files via upload

--- backend/createTable.js
+++ backend/createTable.js
@@ -1,12 +1,10 @@
 import { sql } from './sql.js'
 
 await sql`
-    CREATE TABLE IF NOT EXISTS users(
-        id UUID PRIMARY KEY, 
+    CREATE TABLE IF NOT EXISTS users (
+        id UUID PRIMARY KEY,
         name TEXT NOT NULL,
         email TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL
     );
 `
-
-
